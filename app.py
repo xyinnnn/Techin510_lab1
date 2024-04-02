59a1335271b807b4f9a5f209b276024c208b57e9
尹晓睿 <xyin1@uw.edu>
2024-04-01 20:05:20 -0700

    init

--- app.py
+++ app.py
@@ -1,5 +1,34 @@
 import streamlit as st
 st.title("🧚🏻‍♀️Hi, my name is Ruirui")
-st.header("About Me")
-st.markdown
+st.header("🙋🏻‍♀️About Me")
 
+st.markdown( '''Hi! My name is Xiaorui Yin. Moving to the US at 14, I learned how to embrace diversity, 
+always tried new hobbies and gained the strength to adapt to any situation. 
+
+I loved making new connections and found design as a way to express my passion for life. 
+ ''')
+st.image("/Users/yinxiaorui/Desktop/WechatIMG615.jpg")
+st.header("📚Education")
+
+st.markdown("Artcenter College of Design - Product design major")
+st.divider()
+st.markdown("University of Washington - Master of Science in Technology Innovation")
+
+st.header("🎉Work Experience")
+
+st.markdown("*Ogilvy* : **Industrail Designer** ***(Intern)***.")
+st.divider()
+st.markdown("*Harbour Outdoor* : **Furniture designer** ***(Full Time)***.")
+
+st.header("Hobbies and Interests")
+
+st.markdown(" ''' 🐶 🌷 🧋 🍩 🍰 🍺 ⚽️ 🎾 🥊 🏋🏻‍♀️ 🧗 🎨 🏎 ''' ")
+
+st.header("Interesting Projects")
+
+st.markdown(" As a Product Designer, I want to create connections between users, businesses and brands. ") 
+st.markdown(" Understanding human need, I transform empathetic thinking to new strategies and opportunities in ") 
+st.markdown(" concept development and innovation. Ideation, 3D modeling and prototyping allow me to bring my ") 
+st.markdown(" imagination to life, while visualization allows me to connect the emotional story to the user. ") 
+st.markdown(" Whether it’s CMF, electronics or furniture, I want to design experiences that are human-centered ") 
+st.markdown(" and show people how much I truly care. ")
